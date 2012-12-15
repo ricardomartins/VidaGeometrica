7f6b45186bd61049a8b7a35be751ed9f2804090f
Ricardo Martins <rikhard.martins@gmail.com>
2012-12-15 19:36:57 +0000

    Acabada a classe BlendedAnimation.class e acertada a forma correcta de misturar os sprites

--- VidaGeometrica/src/com/rikmartins/libgdxext/BlendedAnimation.java
+++ VidaGeometrica/src/com/rikmartins/libgdxext/BlendedAnimation.java
@@ -1,32 +1,74 @@
 package com.rikmartins.libgdxext;
 
+import java.math.MathContext;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 
 public class BlendedAnimation extends Animation {
-	public class TransformedTextureRegion {
+	public final float invertedFrameDuration;
 
+	public class TransformedTextureRegion {
+		private final TextureRegion theTextureRegion;
+		private final float alpha;
+
+		public TransformedTextureRegion(TextureRegion theTextureRegion,
+				float alpha) {
+			this.theTextureRegion = theTextureRegion;
+			this.alpha = alpha;
+		}
+
+		public TextureRegion getTheTextureRegion() {
+			return this.theTextureRegion;
+		}
+
+		public float getAlpha() {
+			return this.alpha;
+		}
 	}
 
 	public BlendedAnimation(float frameDuration, List keyFrames) {
 		super(frameDuration, keyFrames);
+		invertedFrameDuration = 1 / frameDuration;
 	}
-	
-	public BlendedAnimation(float frameDuration, List keyFrames, int playType){
+
+	public BlendedAnimation(float frameDuration, List keyFrames, int playType) {
 		super(frameDuration, keyFrames, playType);
+		invertedFrameDuration = 1 / frameDuration;
 	}
 
-	public BlendedAnimation(float frameDuration, TextureRegion... keyFrames){
+	public BlendedAnimation(float frameDuration, TextureRegion... keyFrames) {
 		super(frameDuration, keyFrames);
-	}
-	
-	public List<TextureRegion> getBlendedKeyFrames (float stateTime, boolean looping) {
-		return null;
+		invertedFrameDuration = 1 / frameDuration;
 	}
 
-	public List<TextureRegion> getBlendedKeyFrame (float stateTime) {
+	public List<TransformedTextureRegion> getBlendedKeyFrames(float stateTime,
+			boolean looping) {
+		TextureRegion currentTR = getKeyFrame(stateTime, looping);
+		TextureRegion nextTR = getKeyFrame(stateTime + frameDuration, looping);
+		float nextAlpha = (stateTime - (com.badlogic.gdx.math.MathUtils
+				.floor(stateTime * invertedFrameDuration) * frameDuration))
+				* invertedFrameDuration;
+		float currentAlpha = 1f - nextAlpha;
+
+		//nextAlpha = (float) Math.pow(nextAlpha, 0.3333f);
+		//currentAlpha = (float) Math.pow(currentAlpha, 0.3333f);
+		
+		List<TransformedTextureRegion> result = new ArrayList<TransformedTextureRegion>();
+		if (currentTR == nextTR) {
+			result.add(new TransformedTextureRegion(currentTR, 1f));
+		} else {
+			result.add(new TransformedTextureRegion(currentTR, currentAlpha));
+			result.add(new TransformedTextureRegion(nextTR, nextAlpha));
+		}
+
+		return result;
+	}
+
+	public List<TransformedTextureRegion> getBlendedKeyFrame(float stateTime) {
 		return null;
 	}
 }
--- VidaGeometrica/src/com/rikmartins/vidageometrica/VidaGeometrica.java
+++ VidaGeometrica/src/com/rikmartins/vidageometrica/VidaGeometrica.java
@@ -1,5 +1,7 @@
 package com.rikmartins.vidageometrica;
 
+import java.util.List;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL11;
@@ -9,35 +11,42 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.rikmartins.libgdxext.BlendedAnimation;
+import com.rikmartins.libgdxext.BlendedAnimation.TransformedTextureRegion;
 
 public class VidaGeometrica implements ApplicationListener {
 	private Texture texturas;
 	private SpriteBatch lote;
-	private Animation animadoCiano;
-	private Animation animadoAmarelo;
-	private Animation animadoBranco;
+	private BlendedAnimation animadoCiano;
+	private BlendedAnimation animadoAmarelo;
+	private BlendedAnimation animadoBranco;
 	private Sprite inanimadoCastanho;
 	private Sprite inanimadoVerde;
-	
+
 	private OrthographicCamera camara;
-	
+
 	private float stateTime;
-	
+
 	@Override
 	public void create() {
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
-		
-		camara = new OrthographicCamera(1, h/w);
-		
-		texturas = new Texture(Gdx.files.internal("graficos/texturas.png").toString());
-		TextureRegion[][] regioesTexturas = TextureRegion.split(texturas, texturas.getWidth() / 4, texturas.getHeight() / 4);
-		animadoCiano = new Animation(0.025f, regioesTexturas[0]);
-		animadoAmarelo = new Animation(0.050f, regioesTexturas[1]);
-		animadoBranco = new Animation(0.075f, regioesTexturas[2]);
+
+		camara = new OrthographicCamera(1, h / w);
+
+		texturas = new Texture(Gdx.files.internal("graficos/texturas.png")
+				.toString());
+		TextureRegion[][] regioesTexturas = TextureRegion.split(texturas,
+				texturas.getWidth() / 4, texturas.getHeight() / 4);
+		animadoCiano = new BlendedAnimation(0.025f, regioesTexturas[0]);
+		animadoCiano.setPlayMode(BlendedAnimation.LOOP);
+		animadoAmarelo = new BlendedAnimation(1f, regioesTexturas[1]);
+		animadoAmarelo.setPlayMode(BlendedAnimation.LOOP);
+		animadoBranco = new BlendedAnimation(1f, regioesTexturas[2]);
+		animadoBranco.setPlayMode(BlendedAnimation.LOOP);
 		inanimadoCastanho = new Sprite(regioesTexturas[3][0]);
 		inanimadoVerde = new Sprite(regioesTexturas[3][1]);
-		//inanimadoVerde.
+		// inanimadoVerde.
 		stateTime = 0f;
 		lote = new SpriteBatch();
 	}
@@ -51,21 +60,27 @@ public class VidaGeometrica implements ApplicationListener {
 	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-		
+
 		lote.setProjectionMatrix(camara.combined);
-		
-		stateTime += Gdx.graphics.getDeltaTime();        
+
+		stateTime += Gdx.graphics.getDeltaTime();
+
 
 		inanimadoVerde.setSize(0.5f, 0.5f);
-		inanimadoVerde.setPosition(-inanimadoVerde.getWidth()/2, -inanimadoVerde.getHeight()/2);
-		
-		
-		TextureRegion currentFrame = animadoBranco.getKeyFrame(stateTime, true);
-		
+		inanimadoVerde.setPosition(-inanimadoVerde.getWidth() / 2,
+				-inanimadoVerde.getHeight() / 2);
+
+		List<TransformedTextureRegion> currentFrame = animadoBranco
+				.getBlendedKeyFrames(stateTime, true);
+
 		lote.setProjectionMatrix(camara.combined);
+		lote.setBlendFunction(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
 		lote.begin();
-		//inanimadoVerde.draw(lote);
-		lote.draw(currentFrame, -0.5f, -0.5f, 1f, 1f);
+		// inanimadoVerde.draw(lote);
+		for (TransformedTextureRegion ttr : currentFrame) {
+			lote.setColor(1f, 1f, 1f, ttr.getAlpha());
+			lote.draw(ttr.getTheTextureRegion(), -0.5f, -0.5f, 1f, 1f);
+		}
 		lote.end();
 	}
 
