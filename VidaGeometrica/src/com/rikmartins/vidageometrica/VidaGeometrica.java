38f2ac9b65d7d23e3ab710bcc5a81bb34a520374
Ricardo Martins <rikhard.martins@gmail.com>
2012-12-19 10:22:14 +0000

    Alguns testes com a classe principal, adicionado o .gitignore para evitar mau comportamento nos locais onde se clone o projecto

--- VidaGeometrica/src/com/rikmartins/vidageometrica/VidaGeometrica.java
+++ VidaGeometrica/src/com/rikmartins/vidageometrica/VidaGeometrica.java
@@ -1,18 +1,13 @@
 package com.rikmartins.vidageometrica;
 
-import java.util.List;
-
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL11;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.rikmartins.libgdxext.BlendedAnimation;
-import com.rikmartins.libgdxext.BlendedAnimation.TransformedTextureRegion;
 import com.rikmartins.libgdxext.ExtendedAnimation;
 import com.rikmartins.libgdxext.Frame;
 
@@ -40,31 +35,35 @@ public class VidaGeometrica implements ApplicationListener {
 				.toString());
 		TextureRegion[][] regioesTexturas = TextureRegion.split(texturas,
 				texturas.getWidth() / 4, texturas.getHeight() / 4);
-		Frame[][] sprites = new Frame[regioesTexturas.length][regioesTexturas[0].length];
+		Frame[][] frames = new Frame[regioesTexturas.length][regioesTexturas[0].length];
 		for(int tri = 0; tri < regioesTexturas.length ; tri++){ //TextureRegion[] trArray : regioesTexturas){
 			TextureRegion[] trArray = regioesTexturas[tri];
 			for(int trj = 0; trj < trArray.length; trj++){
-				sprites[tri][trj] = new Frame(trArray[trj]);
+				frames[tri][trj] = new Frame(trArray[trj]);
+				frames[tri][trj].setSize(0.5f, 0.5f);
+				frames[tri][trj].setPosition(-0.25f, -0.25f);
+				frames[tri][trj].setOrigin(0.25f, 0.25f);
+				
 			}
 		}
-		animadoCiano = new ExtendedAnimation(0.025f, sprites[0]);
-		animadoCiano.setPlayMode(BlendedAnimation.LOOP);
-		animadoAmarelo = new ExtendedAnimation(1f, sprites[1]);
-		animadoAmarelo.setPlayMode(BlendedAnimation.LOOP);
-		animadoBranco = new ExtendedAnimation(1f, sprites[2]);
-		animadoBranco.setPlayMode(BlendedAnimation.LOOP);
+		animadoCiano = new ExtendedAnimation(0.1f, frames[0]);
+		animadoCiano.setPlayMode(ExtendedAnimation.LOOP);
+		animadoAmarelo = new ExtendedAnimation(1f, frames[1]);
+		animadoAmarelo.setPlayMode(ExtendedAnimation.LOOP);
+		animadoBranco = new ExtendedAnimation(1f, frames[2]);
+		animadoBranco.setPlayMode(ExtendedAnimation.LOOP);
 		inanimadoCastanho = new Sprite(regioesTexturas[3][0]);
 		inanimadoVerde = new Sprite(regioesTexturas[3][1]);
 		// inanimadoVerde.
 		stateTime = 0f;
 		lote = new SpriteBatch();
 	}
-
+	
 	@Override
 	public void dispose() {
 		texturas.dispose();
 	}
-
+	
 	@Override
 	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0, 0);
@@ -73,17 +72,16 @@ public class VidaGeometrica implements ApplicationListener {
 		lote.setProjectionMatrix(camara.combined);
 
 		stateTime += Gdx.graphics.getDeltaTime();
-
+		
 		Sprite ciano = animadoCiano.getKeyFrame(stateTime, true);
-		ciano.setSize(0.5f, 0.5f);
 		//ciano.setPosition(-ciano.getWidth() / 2,
 		//		-ciano.getHeight() / 2);
-		// ciano.setRotation(stateTime);
-
+		// ciano.setRotation(stateTime * 10);
 		
-		inanimadoVerde.setSize(0.5f, 0.5f);
-		inanimadoVerde.setPosition(-inanimadoVerde.getWidth() / 2,
-				-inanimadoVerde.getHeight() / 2);
+		
+		//inanimadoVerde.setSize(0.5f, 0.5f);
+		//inanimadoVerde.setPosition(-inanimadoVerde.getWidth() / 2,
+		//		-inanimadoVerde.getHeight() / 2);
 
 		lote.setProjectionMatrix(camara.combined);
 		lote.setBlendFunction(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
